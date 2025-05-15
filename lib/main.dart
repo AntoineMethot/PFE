837b8a1cca12abd9e01e9e53355fa9f16d8ff7d7
Antoine Methot <amethot@etu.uqac.ca>
2025-05-14 20:32:53 -0400

    Starting to get something going. basic screen with XYZ and numbers but its all static. Will need to put stateful in there I think

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
-import 'package:sensors_plus/sensors_plus.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BLE Accelerometer App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const BLEAccelerometerScreen(),
-    );
-  }
-}
-
-class BLEAccelerometerScreen extends StatefulWidget {
-
-  const BLEAccelerometerScreen({super.key});
-
-  @override
-  State<BLEAccelerometerScreen> createState() => _BLEAccelerometerScreenState();
-}
-
-class _BLEAccelerometerScreenState extends State<BLEAccelerometerScreen> {
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('BLE Accelerometer App'),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          centerTitle: true,
+          title: Text('ACCELEROMETER DATA'),
+        ),
+        body:  Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('X: ', style: TextStyle(fontSize: 18)),
+                  Text('0.00', style: const TextStyle(fontSize: 18)), // Replace '0.00' with dynamic data
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Y: ', style: TextStyle(fontSize: 18)),
+                  Text('0.00', style: const TextStyle(fontSize: 18)), // Replace '0.00' with dynamic data
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Z: ', style: TextStyle(fontSize: 18)),
+                  Text('0.00', style: const TextStyle(fontSize: 18)), // Replace '0.00' with dynamic data
+                ],
+              ),
+            ],
+          ),
+        )       // Replace the Center widget in your body with this Column widget
+        
       ),
     );
   }
